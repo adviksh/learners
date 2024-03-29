0935bf1557c32de32146b92192d8d2f69300daf8
Advik Shreekumar <advikshreekumar@gmail.com>
2024-03-29 16:47:33 -0400

    bugfixes

--- R/binary_logistic.R
+++ R/binary_logistic.R
@@ -5,25 +5,10 @@
 #' with class 'learner'.
 binary_logistic <- structure(
   class = c("learner_constructor", "function"),
-  function() {
+  function(engine = stats::glm.fit) {
     make_learner(
       name = "binary_logistic",
-      train_fun = function(features, tgt, wt, engine = stats::glm.fit) {
-
-        if (is.factor(tgt)) tgt <- as.integer(tgt) - 1L
-
-        tgt <- as.matrix(tgt)
-
-        model <- engine(x = cbind(1, features),
-                        y = tgt,
-                        weights = wt,
-                        family = stats::binomial())
-
-        model_coef <- stats::coef(model)
-        model_coef[is.na(model_coef)] <- 0
-
-        return(model_coef)
-      },
+      train_fun = purrr::partial(binary_logistic_train, engine = engine),
       predict_fun = function(model, features) {
         z <- as.numeric(model[1] + (features %*% model[-1]))
         stats::plogis(z)
@@ -31,3 +16,21 @@ binary_logistic <- structure(
     )
   }
 )
+
+binary_logistic_train = function(features, tgt, wt = NULL, engine) {
+
+  if (is.null(wt)) wt = rep(1, length(tgt))
+  if (is.factor(tgt)) tgt <- as.integer(tgt) - 1L
+
+  tgt <- as.matrix(tgt)
+
+  model <- engine(x = cbind(1, features),
+                  y = tgt,
+                  weights = wt,
+                  family = stats::binomial())
+
+  model_coef <- stats::coef(model)
+  model_coef[is.na(model_coef)] <- 0
+
+  return(model_coef)
+}
--- R/regression_ols.R
+++ R/regression_ols.R
@@ -3,30 +3,33 @@
 #'
 #' @return A linear regression model for continuous outcomes,
 #' with class 'learner'.
-regr_ols <- structure(
+regr_ols = structure(
   class = c("learner_constructor", "function"),
-  function() {
+  function(engine = stats::glm.fit) {
     make_learner(
       name = "regr_ols",
-      train_fun = function(features, tgt, wt, engine = stats::glm.fit) {
-
-        if (is.factor(tgt)) tgt <- as.integer(tgt) - 1L
-
-        tgt <- as.matrix(tgt)
-
-        model <- engine(x = cbind(1, features),
-                        y = tgt,
-                        weights = wt,
-                        family = stats::gaussian())
-
-        model_coef <- stats::coef(model)
-        model_coef[is.na(model_coef)] <- 0
-
-        return(model_coef)
-      },
+      train_fun = purrr::partial(regr_ols_train, engine = engine),
       predict_fun = function(model, features) {
         as.numeric(model[1] + (features %*% model[-1]))
       }
     )
   }
 )
+
+regr_ols_train = function(features, tgt, wt = NULL, engine) {
+
+  if (is.null(wt)) wt = rep(1, length(tgt))
+  if (is.factor(tgt)) tgt = as.integer(tgt) - 1L
+
+  tgt = as.matrix(tgt)
+
+  model = engine(x = cbind(1, features),
+                 y = tgt,
+                 weights = wt,
+                 family = stats::gaussian())
+
+  model_coef = stats::coef(model)
+  model_coef[is.na(model_coef)] = 0
+
+  return(model_coef)
+}
