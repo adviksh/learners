b733581a21c6968aa6197ff95104d7522c4ac309
adviksh <8152036+adviksh@users.noreply.github.com>
2024-10-10 10:26:59 -0400

    Update regression_elasticnet.R
    lessons from binary elasticnet

--- R/regression_elasticnet.R
+++ R/regression_elasticnet.R
@@ -8,46 +8,81 @@
 #' with class 'learner'.
 regr_elasticnet <- structure(
   class = c("learner_constructor", "function"),
-  function(alpha = seq(0, 1, length.out = 5)) {
+  function(alpha = seq(0, 1, length.out = 5), workers = 1) {
 
     make_learner(name        = "regr_elasticnet",
                  tune_fun    = purrr::partial(regr_elasticnet_tune,
-                                              alpha = alpha),
-                 predict_fun = regr_elasticnet_predict)
+                                              alpha = alpha,
+                                              workers = workers),
+                 predict_fun = regr_elasticnet_predict,
+                 predict_tuned_fun = regr_elasticnet_predict_tuned)
   }
 )
 
 
 # Methods -----------------------------------------------------------------
 regr_elasticnet_tune <- function(features, tgt, wt = rep(1, nrow(features)),
-                                 tune_folds, alpha) {
+                                 tune_folds, alpha, workers) {
 
   tune_folds <- match(tune_folds, sort(unique(tune_folds)))
 
-  cv_fits <- purrr::map(alpha,
-                        ~glmnet::cv.glmnet(x = features,
-                                           y = tgt,
-                                           weights = wt,
-                                           foldid  = tune_folds,
-                                           alpha   = .x))
+  if (workers > 1) {
+    parallel = TRUE
+    doFuture::registerDoFuture()
+    future::plan(future::multicore, workers = workers)
+  } else {
+    parallel = FALSE
+  }
 
-  tidy_fit <- function(fit, alpha) {
+  max_vars = min(min(table(tune_folds)) - 1,
+                 ncol(features))
+
+  cv_fits = purrr::map(alpha,
+                       ~glmnet::cv.glmnet(x = features,
+                                          y = tgt,
+                                          weights = wt,
+                                          foldid  = tune_folds,                                        
+                                          alpha   = .x,
+                                          keep    = TRUE,
+                                          parallel = parallel,
+                                          pmax     = max_vars))
+
+  tidy_fit = function(fit, alpha) {
 
     data.frame(alpha    = alpha,
                lambda   = fit$lambda,
                loss      = fit$cvm)
   }
 
-  hyperparams <- purrr::map2(cv_fits, alpha, tidy_fit)
+  extract_best_predictions = function(fits) {
+    # Get the lowest MSE from each regression
+    glmnet_cvm = purrr::map_dbl(fits, function(f) { min(f$cvm) })
+
+    # Figure out which index in the output corresponds to predictions from the best fit
+    best_fit = fits[[ which.min(glmnet_cvm) ]]
+    lambda_min_idx = match(best_fit$lambda.min,
+                           best_fit$lambda)
+
+    # Return
+    z_hat = best_fit$fit.preval[, lambda_min_idx]
+    as.numeric(plogis(z_hat))
+  }
+
+  hyperparams = purrr::map2(cv_fits, alpha, tidy_fit)
   hyperparams = purrr::list_rbind(hyperparams)
 
-  best_hyperparam_row <- which.min(hyperparams$loss)
-  best_mod_idx        <- match(hyperparams$alpha[best_hyperparam_row], alpha)
+  best_hyperparam_row = which.min(hyperparams$loss)
+  best_mod_idx        = match(hyperparams$alpha[best_hyperparam_row], alpha)
 
-  purrr::partial(regr_elasticnet_train,
-                 lambda  = cv_fits[[best_mod_idx]]$lambda,
-                 s       = hyperparams$lambda[best_hyperparam_row],
-                 alpha   = hyperparams$alpha[best_hyperparam_row])
+  tune_res = list(
+    tuned_model = extract_best_predictions(cv_fits),
+    train_fun   = purrr::partial(regr_elasticnet_train,
+                                 lambda  = cv_fits[[best_mod_idx]]$lambda,
+                                 s       = hyperparams$lambda[best_hyperparam_row],
+                                 alpha   = hyperparams$alpha[best_hyperparam_row])
+  )
+
+  tune_res
 }
 
 regr_elasticnet_train <- function(features, tgt, wt, lambda, s, alpha) {
@@ -66,3 +101,6 @@ regr_elasticnet_predict <- function(model, features) {
                             type = "response"))
 }
 
+regr_elasticnet_predict_tuned = function(model, features, tune_folds) {
+  return(model)
+}
